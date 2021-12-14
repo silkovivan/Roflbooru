06cbc18d772f297f8f79a4ccc9456f42c89e5b00
SanOK1999 <95934210+SanOK1999@users.noreply.github.com>
2021-12-14 23:13:47 +0300

    Add files via upload

---
+++ mems/generation/post-mems.php
@@ -0,0 +1,17 @@
+<?php require_once("$_SERVER[DOCUMENT_ROOT]/mems/generation/pagination-mems.php"); ?>
+
+<?php  
+
+foreach ($data as $results):
+
+?>
+
+<article class="article-featured">
+               <h2 class="article-title"><?php echo $results["title"] ?></h2>
+               <img src="<?php echo $results["thumbnail"] ?>" class="article-image">
+               <p class="article-info"><?php echo date("d/m/Y", strtotime($results["date"]));?></p>
+               <p class="article-body"><?php echo $string = mb_substr($results["content"], 0, 150); ?></p>
+               <a href="post.php?id=<?php echo $results["id"] ?>" class="article-read-more">Открыть пост</a>
+</article>
+
+<?php endforeach; ?>
